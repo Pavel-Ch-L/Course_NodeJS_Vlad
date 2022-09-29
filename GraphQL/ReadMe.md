9d6eb5e3cd960818f74a732b23ec6359ad6b7f66
Pavel_Ch__(L) <pavel79.su@gmail.com>
2022-09-29 20:26:31 +0600

    2 graphql

--- GraphQL/ReadMe.md
+++ GraphQL/ReadMe.md
@@ -4,10 +4,17 @@
 	• Скопировать проект Rest
 
 ### 2. Настройка GraphQL
-	• 
+	• npm install express-graphql graphql
+	• Удалить роуты из index.js
+	• Подключить и сконфигурировать express-graphql в index.js
+	• Создать в папке ./graphql schema.js, resolver.js
+	• Написать схему в schema.js и функцию test() в resolver.js
+
+### 3. Как работает GraphQL
+	•
 
 ***
 &emsp;
 # Информация  
 	• https://graphql.org/
-	• 
+	• test: String! //! - обязательный тип
--- GraphQL/index.js
+++ GraphQL/index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const path = require('path')
-const todoRoutes = require('./routes/todo')
 const sequelize = require('./utils/database')
 const app = express()
+const {graphqlHTTP} = require('express-graphql')
+const schema = require('./graphql/schema')
+const resolver = require('./graphql/resolver')
 const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 
-app.use('/api/todo', todoRoutes)
+app.use(graphqlHTTP({
+  schema: schema,
+  rootValue: resolver
+}))
 
 app.use((req, res, next) => {
   res.sendFile('/index.html')
